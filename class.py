7dbb0770185fbc8e14c874fcba7bfce0c5d80945
LUCKYRAI777 <luckyraiff123@gmail.com>
2025-09-18 17:08:39 +0530

    Add files via upload

---
+++ class.py
@@ -0,0 +1,13 @@
+class stud:
+    def __init__(self, a, b, c):
+        self.sno = a
+        self.name = b
+        self.age = c
+
+    def disp(self):
+        print(self.sno)
+        print(self.name)
+        print(self.age)
+
+ob = stud(35, "lucky", 17)
+ob.disp()
